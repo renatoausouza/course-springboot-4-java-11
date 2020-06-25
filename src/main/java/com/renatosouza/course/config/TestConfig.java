3fb7d50df356577a183c2556ad2120f5d877e5a3
Renato Souza <renato.souza92@gmail.com>
2020-06-24 22:47:40 -0300

    Payment, one-to-one association

--- src/main/java/com/renatosouza/course/config/TestConfig.java
+++ src/main/java/com/renatosouza/course/config/TestConfig.java
@@ -11,6 +11,7 @@ import org.springframework.context.annotation.Profile;
 import com.renatosouza.course.entities.Category;
 import com.renatosouza.course.entities.Order;
 import com.renatosouza.course.entities.OrderItem;
+import com.renatosouza.course.entities.Payment;
 import com.renatosouza.course.entities.Product;
 import com.renatosouza.course.entities.User;
 import com.renatosouza.course.entities.enums.OrderStatus;
@@ -80,5 +81,9 @@ public class TestConfig implements CommandLineRunner{
 		OrderItem oi3 = new OrderItem(o2, p3, 2, p3.getPrice());
 		OrderItem oi4 = new OrderItem(o3, p5, 2, p5.getPrice());
 		orderItemRepository.saveAll(Arrays.asList(oi1, oi2, oi3, oi4));
+		
+		Payment pay1 = new Payment(null, Instant.parse("2019-06-20T21:53:07Z"), o1);
+		o1.setPayment(pay1);
+		orderRepository.save(o1);
 	}
 }
---
+++ src/main/java/com/renatosouza/course/entities/Payment.java
@@ -0,0 +1,86 @@
+package com.renatosouza.course.entities;
+
+import java.io.Serializable;
+import java.time.Instant;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.MapsId;
+import javax.persistence.OneToOne;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "tb_payment")
+public class Payment implements Serializable {
+	private static final long serialVersionUID = 1L;
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long Id;
+	private Instant moment;
+	
+	@OneToOne
+	@MapsId
+	private Order order;
+	
+	public Payment() {}
+
+	public Payment(Long id, Instant moment, Order order) {
+		Id = id;
+		this.moment = moment;
+		this.order = order;
+	}
+	
+	public long getId() {
+		return Id;
+	}
+
+	public void setId(long id) {
+		Id = id;
+	}
+
+	public Instant getMoment() {
+		return moment;
+	}
+
+	public void setMoment(Instant moment) {
+		this.moment = moment;
+	}
+
+	public Order getOrder() {
+		return order;
+	}
+
+	public void setOrder(Order order) {
+		this.order = order;
+	}
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + (int) (Id ^ (Id >>> 32));
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Payment other = (Payment) obj;
+		if (Id != other.Id)
+			return false;
+		return true;
+	}
+
+	@Override
+	public String toString() {
+		return "Payment [Id=" + Id + ", moment=" + moment + ", order=" + order + "]";
+	}
+}
